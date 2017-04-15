3d312a15eb00d6e5cdc8faa5a69867e9cb8c552a
diogogomes77 <diogo.gomes77@gmail.com>
2017-04-15 17:44:10 +0100

    add TPI

---
+++ ExemplosProf/LocationUpdates/app/src/main/java/edu/cuby/diogo/locationupdates/MainActivity.java
@@ -0,0 +1,111 @@
+package edu.cuby.diogo.locationupdates;
+
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends ActionBarActivity {
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager)
+                this.getSystemService(Context.LOCATION_SERVICE);
+// Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            public void onProviderEnabled(String provider) {}
+            public void onProviderDisabled(String provider) {}
+        };
+        //decidir qual a precisão necessária
+// Register the listener with the Location Manager to receive location updates
+        //      String locationProvider = LocationManager.NETWORK_PROVIDER;
+// Or, use GPS location data:
+        String locationProvider = LocationManager.GPS_PROVIDER;
+        //Quando efectivamente começar a detectar
+        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+        setContentView(R.layout.activity_main);
+    }
+    private void makeUseOfNewLocation(Location location) {
+        //Mostrar localizacao
+        TextView posicao = new TextView(this);
+        posicao.setText(location.getLatitude()+","+location.getLongitude()
+                +","+location.getAltitude());
+        setContentView(posicao);
+    }
+
+
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
+
+}
---
+++ TP1_CUbi_2016_17/app/src/main/java/pt/isec/cubi/tp1_cubi_2016_17/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.isec.cubi.tp1_cubi_2016_17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
